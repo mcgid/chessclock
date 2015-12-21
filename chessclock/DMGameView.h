1f7ddfb3ad95c3c8fbe814e436cc39e59b7574f6
Dan McGinnis <dan@mcgid.ca>
2015-12-20 22:56:28 -0500

    Add external API method declarations

--- chessclock/DMGameView.h
+++ chessclock/DMGameView.h
@@ -6,8 +6,48 @@
 //
 //
 
+#import "DMClock.h"
+
 @interface DMGameView : UIView
 
+- (void)updateWithWhiteTime:(DMClockTime)time;
+- (void)updateWithBlackTime:(DMClockTime)time;
 
+- (void)enableWhiteButton;
+- (void)disableWhiteButton;
+
+- (void)enableBlackButton;
+- (void)disableBlackButton;
+
+- (void)showSliders;
+- (void)hideSliders;
+
+- (void)enablePlayerButtonInteraction;
+- (void)disablePlayerButtonInteraction;
+
+- (void)showResetButton;
+- (void)hideResetButton;
+
+- (void)showTimesButton;
+- (void)hideTimesButton;
+
+- (void)showPauseButton;
+- (void)hidePauseButton;
+
+- (void)showStartGameLabel;
+- (void)hideStartGameLabel;
+
+- (void)resetStartGameLabelContent;
+- (void)replaceStartGameLabelContent;
+
+- (void)showConfirmResetArea;
+- (void)hideConfirmResetArea;
+
+- (void)resetPlayerButtonColors;
+
+- (void)selectPauseButton;
+- (void)deselectPauseButton;
+- (void)selectTimesButton;
+- (void)deselectTimesButton;
 
 @end
