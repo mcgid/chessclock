dcb0a93ea0927e6e80d3284b1efcea09e8839c0b
Dan McGinnis <dan@mcgid.ca>
2015-12-24 14:51:12 -0500

    Add WhiteTurn state UI changes

--- chessclock/States/DMWhiteTurnState.m
+++ chessclock/States/DMWhiteTurnState.m
@@ -10,6 +10,28 @@
 
 @implementation DMWhiteTurnState
 
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
 
+    if ([previousState isKindOfClass:[DMNewGameState class]]) {
+        [self.gameView showPauseButton];
+        [self.gameView showResetButton];
+        [self.gameView hideTimesButton];
+        [self.gameView enablePauseButton];
+        [self.gameView disableResetButton];
+
+        [self.gameView enableWhiteButton];
+    }
+    else if ([previousState isKindOfClass:[DMPausedState class]]) {
+        [self.gameView enableWhiteButton];
+        [self.gameView disableResetButton];
+        [self.gameView deselectPauseButton];
+    }
+    else if ([previousState isKindOfClass:[DMBlackTurnState class]]) {
+        [self.gameView enableWhiteButton];
+        [self.gameView disableBlackButton];
+    }
+}
 
 @end
