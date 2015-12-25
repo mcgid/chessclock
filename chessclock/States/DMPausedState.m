b0261a87883c66881eeb02d85b58bb177bea0057
Dan McGinnis <dan@mcgid.ca>
2015-12-24 20:59:40 -0500

    Add paused state UI changes

--- chessclock/States/DMPausedState.m
+++ chessclock/States/DMPausedState.m
@@ -10,6 +10,51 @@
 
 @implementation DMPausedState
 
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
 
+//    if (self.game.state == DMWhiteTurn) {
+//        self.game.state = DMWhitePaused;
+//        [self.white pause];
+//
+
+//
+//        [self updateInterfaceWithChanges:^{
+//            [self disableWhiteButton];
+//
+//            if (self.whitePlayerHasEndedFirstTurn == NO) {
+//                [self hideStartGameLabel];
+//            }
+//
+//            [self.view layoutIfNeeded];
+//        }];
+//    }
+//    else if (self.game.state == DMBlackTurn) {
+//        self.game.state = DMBlackPaused;
+//        [self.black pause];
+//
+//
+//        [self updateInterfaceWithChanges:^{
+//            [self disableBlackButton];
+//
+//            [self.view layoutIfNeeded];
+//        }];
+//    }
+
+    [self.gameView selectPauseButton];
+    [self.gameView enableResetButton];
+
+    if ([previousState isKindOfClass:[DMWhiteTurnState class]]) {
+        [self.gameView disableWhiteButton];
+    }
+    else if ([previousState isKindOfClass:[DMBlackTurnState class]]) {
+        [self.gameView disableBlackButton];
+    }
+    else if ([previousState isKindOfClass:[DMConfirmResetState class]]) {
+        [self.gameView hideConfirmResetArea];
+    }
+    else if ([previousState isKindOfClass:[DMLoadingState class]]) {
+    }
+}
 
 @end
