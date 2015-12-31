83e18f2a8c726f3030e1900c5184d1b84fd75016
Dan McGinnis <dan@mcgid.ca>
2015-12-31 11:20:50 -0500

    Add black turn state

--- chessclock/States/DMBlackTurnState.m
+++ chessclock/States/DMBlackTurnState.m
@@ -8,8 +8,47 @@
 
 #import "DMStates.h"
 
+#import "DMGame.h"
+#import "DMGameView.h"
+
 @implementation DMBlackTurnState
 
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
+    [super didEnterWithPreviousState:previousState];
+
+    // Start the black clock
+    [self.game.black start];
+
+    [self.game.view enableBlackButton];
+
+    if ([previousState isKindOfClass:[DMWhiteTurnState class]]) {
+        [self.game.view disableWhiteButton];
+    }
+}
+
+- (void)willExitWithNextState:(GKState *)nextState
+{
+    [super willExitWithNextState:nextState];
+
+    // Stop the white clock
+    [self.game.black stop];
+
+    [self.game.view disableBlackButton];
+}
+
+- (void)updateWithDeltaTime:(NSTimeInterval)seconds
+{
+    DMClockTime blackTime = [self.game.black remainingTime];
+
+    if (blackTime.totalSeconds <= 0) {
+        self.game.state = [DMWhiteLostState class];
+    } else if (blackTime.minutes != self.displayedTime.minutes ||
+               blackTime.seconds != self.displayedTime.seconds) {
+        [self.game.view updateWithWhiteTime:blackTime];
+        self.displayedTime = blackTime;
+    }
+}
 
 
 @end
