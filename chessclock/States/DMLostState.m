d10472e0250057a298ccd09b3bde321804ffe4f6
Dan McGinnis <dan@mcgid.ca>
2016-01-01 13:32:39 -0500

    Add Lost state logic

--- chessclock/States/DMLostState.m
+++ chessclock/States/DMLostState.m
@@ -8,6 +8,34 @@
 
 #import "DMStates.h"
 
+@import AudioToolbox;
+
+#import "DMGame.h"
+#import "DMGameView.h"
+
 @implementation DMLostState
+
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
+    if ([previousState isKindOfClass:[DMLoadingState class]]) {
+        [self.game.view showResetButton];
+    } else if ([previousState isKindOfClass:[DMTurnState class]]) {
+        [self.game.view hidePauseButton];
+    }
+
+    [self.game.view enableResetButton];
+    [self.game.view enableWhiteButton];
+    [self.game.view enableBlackButton];
+
+    // Vibrate the device, if possible
+    AudioServicesPlayAlertSound(kSystemSoundID_Vibrate);
+}
+
+- (void)willExitWithNextState:(GKState *)nextState
+{
+    [self.game.view resetPlayerButtonColors];
+    [self.game.view disableBlackButton];
+    [self.game.view hideResetButton];
+}
 
 @end
