d4b6ce0f3ac3cfd80dd77ca87e070268ab4b6da9
Dan McGinnis <dan@mcgid.ca>
2016-01-01 19:32:40 -0500

    Add logic for Settings state

--- chessclock/States/DMSettingsState.m
+++ chessclock/States/DMSettingsState.m
@@ -8,8 +8,33 @@
 
 #import "DMStates.h"
 
+#import "DMGame.h"
+#import "DMGameView.h"
+
 @implementation DMSettingsState
 
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
+    if ([previousState isKindOfClass:[DMLoadingState class]]) {
+        [self.game.view showTimesButton];
+    }
 
+    [self.game.view selectTimesButton];
+    [self.game.view showSliders];
+    [self.game.view enableBlackButton];
+    [self.game.view hideStartGameLabel];
+
+    [self.game.view disablePlayerButtonInteraction];
+}
+
+- (void)willExitWithNextState:(GKState *)nextState
+{
+    [self.game.view enablePlayerButtonInteraction];
+
+    [self.game.view showStartGameLabel];
+    [self.game.view disableBlackButton];
+    [self.game.view hideSliders];
+    [self.game.view deselectTimesButton];
+}
 
 @end
