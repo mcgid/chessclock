a2be2620eb39691a8362860acd941670d1ce39e9
Dan McGinnis <dan@mcgid.ca>
2016-01-06 14:33:24 -0500

    Make view property in GameViewAnimator a weak ref
    The view shouldn't be unnecessarily retained.

--- chessclock/DMGameViewAnimator.h
+++ chessclock/DMGameViewAnimator.h
@@ -11,7 +11,7 @@
 
 @interface DMGameViewAnimator : NSObject <DMInterfaceTransitioning>
 
-@property (nonatomic, readonly) DMGameView *view;
+@property (nonatomic, weak, readonly) DMGameView *view;
 
 // Designated initializer
 - (instancetype)initWithView:(DMGameView *)view;
