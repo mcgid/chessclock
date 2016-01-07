7cd66093f4765bea26e93c0ca6f3bc001f5a8906
Dan McGinnis <dan@mcgid.ca>
2016-01-07 18:52:38 -0500

    Update interface with current clock time at load

--- chessclock/States/DMLoadingState.m
+++ chessclock/States/DMLoadingState.m
@@ -12,6 +12,10 @@
 
 - (void)willExitWithNextState:(GKState *)nextState
 {
+    // Regardless of where we're going, put the current clock times up
+    [self.game.interface interfaceShouldUpdateWithWhiteTime:[self.game.white remainingTime]];
+    [self.game.interface interfaceShouldUpdateWithBlackTime:[self.game.black remainingTime]];
+
     if ([nextState isKindOfClass:[DMNewGameState class]]) {
         [self.game.interface interfaceShouldTransitionFromLoadingToNewGame:self];
     } else if ([nextState isKindOfClass:[DMSettingsState class]]) {
