55f0b5f119d4d6a5e29f0fe3a64db9184a3a58dc
Dan McGinnis <dan@mcgid.ca>
2016-01-08 16:46:43 -0500

    Make the NewGame state actually reset the clocks

--- chessclock/States/DMNewGameState.m
+++ chessclock/States/DMNewGameState.m
@@ -13,6 +13,15 @@
 
 @implementation DMNewGameState
 
+- (void)didEnterWithPreviousState:(GKState *)previousState
+{
+    [self.game.white reset];
+    [self.game.black reset];
+
+    [self.game.interface interfaceShouldUpdateWithWhiteTime:[self.game.white remainingTime]];
+    [self.game.interface interfaceShouldUpdateWithBlackTime:[self.game.black remainingTime]];
+}
+
 - (void)willExitWithNextState:(GKState *)nextState
 {
     if ([nextState isKindOfClass:[DMSettingsState class]]) {
